30468e6c9ce634592af07332dfff956ef25fc2da
Richard Castillo <armed_deity@hotmail.com>
2020-03-11 17:06:18 -0700

    answered questions

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
 // 1. Write a console.log() statement that answers the following question: What worked
 // well for you over the last 9 weeks that aided in your learning process?
-
-console.log(
-  "What worked well for you over the last 9 weeks that aided in your learning process?"
-);
-console.log("");
+console.log("Heavy use of the internet, mainly stack overflow and youtube videos. Heavy use of demos are better, seeing code in action puts a better perspective on what to do.");
 
 // 2. Write a console.log() statement that answers the following question: What would you
 // change about the course?
 
 console.log("What would you change about the course?");
-console.log("");
+console.log("More use of demos. One day theory and lecture, and one day a lab");
 
 // 3. Write a console.log() statement that answers the following question: Did you learn
 // what you expected to learn in this course?
 
-console.log("Did you learn what you expected to learn in this course?");
-console.log("");
+console.log("I did. A lot of the learning is just that basic push to implement JS. the real learning is the projects outside class.");
 
 // 4. Write a console.log() statement that answers the following question: What concepts
 // were new to you and what concepts had you encountered already in the past?
 
-console.log(
-  "What concepts were new to you and what concepts had you encountered already in the past?"
-);
-console.log("");
+console.log("mainly data-structures, and more of a deeper understanding of loops.");
